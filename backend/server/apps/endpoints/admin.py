17a079dd2eba97fb275473acb6da28a82f83f3c5
Betiku Oluwatobi <anthonytobynho@gmail.com>
2023-03-17 09:49:08 +0100

    Delete backend/server directory

--- backend/server/apps/endpoints/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Users_Input
-# Register your models here.
-
-@admin.register(Users_Input)
-class UsersInputAdmin(admin.ModelAdmin):
-  list_display = ['users_input']
-
--- backend/server/apps/endpoints/models.py
+++
@@ -1,4 +0,0 @@
-from django.db import models
-
-class Users_Input(models.Model):
-  users_input = models.TextField(max_length=300)
